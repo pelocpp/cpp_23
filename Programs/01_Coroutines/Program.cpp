992c037e58997efaf56dd709a718fdd93f455109
Peter Loos <cpp@peterloos.de>
2025-06-30 15:19:54 +0200

    continued work

--- Programs/01_Coroutines/Program.cpp
+++ Programs/01_Coroutines/Program.cpp
@@ -7,17 +7,19 @@ void coroutines_02();
 void coroutines_03();
 void coroutines_04();
 void coroutines_05();
-
 void coroutines_08();
+void coroutines_10();
 
 int main()
 {
     //coroutines_01();
-    coroutines_02();
+    //coroutines_02();
     //coroutines_03();
     //coroutines_04();
     //coroutines_05();
     //coroutines_08();
+
+    coroutines_10();
 
     return 0;
 }
